4ce297ae19b142a2752fd98564ee7fb40f9e95d6
Steven Alexander <alexgrant200@yahoo.com>
2022-05-05 01:18:36 +0700

    init s2

---
+++ guessNumber-s2/screens/StartGameScreen.jsx
@@ -0,0 +1,15 @@
+import { View, TextInput, StyleSheet } from 'react-native';
+import PrimaryButton from '../components/PrimaryButton';
+
+function StartGameScreen() {
+  return (
+    <View>
+      <TextInput />
+      <PrimaryButton>Test</PrimaryButton>
+    </View>
+  );
+}
+
+export default StartGameScreen;
+
+const styles = StyleSheet.create({});
